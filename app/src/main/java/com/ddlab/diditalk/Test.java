790bbbed1f8e0aa79bc3fd9feb52fb36a9851fec
heebin <heebinee@gmail.com>
2016-04-30 15:19:58 +0900

    Rating btn to dondongpang.xml insert Test.class

---
+++ app/src/main/java/com/ddlab/diditalk/Test.java
@@ -0,0 +1,18 @@
+package com.ddlab.diditalk;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by heedong on 16. 4. 30..
+ */
+public class Test extends FrameLayout{
+    public Test(Context context) {
+        super(context);
+    }
+
+    public Test(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
